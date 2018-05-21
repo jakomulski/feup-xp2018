b8296d513a294935b2ce4bcfac8cdc50620920bf
nonecover <adam.jakomulski@gmail.com>
2018-05-21 18:37:05 +0100

    For notifications

---
+++ app/src/main/java/com/asso/conference/webClient/models/XpEvent.java
@@ -0,0 +1,15 @@
+package com.asso.conference.webClient.models;
+
+import java.util.Date;
+
+public class XpEvent {
+    public String FIELD1;
+    public String FIELD2;
+    public Date FIELD3;
+    public Date FIELD4;
+    public String FIELD5;
+    public String FIELD6;
+    public String FIELD7;
+    public String FIELD8;
+    public String FIELD9;
+}
