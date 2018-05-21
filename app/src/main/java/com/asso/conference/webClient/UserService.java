4e759cfcfabb1d61353a0f1c289bb21c468a2e69
Telmo Barros <telmo.20@gmail.com>
2018-05-21 12:26:03 +0100

    working with githubjson for bluetoothDevices

--- app/src/main/java/com/asso/conference/webClient/UserService.java
+++ app/src/main/java/com/asso/conference/webClient/UserService.java
@@ -80,16 +80,17 @@ public enum UserService {
 
     public void getBluetoothDevices(final BookmarkCallback<BluetoothDeviceModel[]> callback){
 
-        BluetoothDeviceModel midi = new BluetoothDeviceModel("C4:BE:84:49:DD:7E", 1, 0.3f, 0.1f);
+        /*BluetoothDeviceModel midi = new BluetoothDeviceModel("C4:BE:84:49:DD:7E", 1, 0.3f, 0.1f);
         BluetoothDeviceModel sensorTag = new BluetoothDeviceModel("B0:B4:48:BC:E5:82", 2,0.5f,0.9f);
         BluetoothDeviceModel bt100 = new BluetoothDeviceModel("A0:E9:DB:08:49:C2", 3,0.9f,0.5f);
 
-        BluetoothDeviceModel[] bts = new BluetoothDeviceModel[] {midi, sensorTag, bt100};
+        BluetoothDeviceModel[] bts = new BluetoothDeviceModel[] {midi, sensorTag, bt100};*/
         Call<ResponseModel<BluetoothDeviceModel[]>> call = gitHubService.getBluetoothDevices();
         call.enqueue(new Callback<ResponseModel<BluetoothDeviceModel[]>>() {
             @Override
             public void onResponse(Call<ResponseModel<BluetoothDeviceModel[]>> call, Response<ResponseModel<BluetoothDeviceModel[]>> response) {
-                Log.d("BT DEVICES", response);
+                BluetoothDeviceModel[] bts = response.body().bluetoothDevices;
+                Log.d("BT DEVICES", bts[0].address);
                 callback.onSuccess(bts); // TODO REMOVE HARDCODED
                 /*if(response.isSuccessful())
                     callback.onSuccess(response.body().success);
@@ -106,8 +107,9 @@ public enum UserService {
 
             @Override
             public void onFailure(Call<ResponseModel<BluetoothDeviceModel[]>> call, Throwable t) {
-                //callback.onError(t.getMessage());
-                callback.onSuccess(bts); // TODO REMOVE HARDCODED
+                callback.onError(t.getMessage());
+                //Log.d("BT DEVICES", t.getMessage());
+                //callback.onSuccess(bts); // TODO REMOVE HARDCODED
             }
         });
     }
--- app/src/main/java/com/asso/conference/webClient/models/ResponseModel.java
+++ app/src/main/java/com/asso/conference/webClient/models/ResponseModel.java
@@ -2,5 +2,6 @@ package com.asso.conference.webClient.models;
 
 public class ResponseModel<T> {
     public T success;
+    public BluetoothDeviceModel[] bluetoothDevices;
     public String failure;
 }
