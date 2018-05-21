e41639eb2edf184fda445bd8fcff2dd7cf0023a1
nonecover <adam.jakomulski@gmail.com>
2018-05-22 00:30:06 +0100

    - xp events

--- app/src/main/java/com/asso/conference/common/DateDeserializer.java
+++ app/src/main/java/com/asso/conference/common/DateDeserializer.java
@@ -5,7 +5,9 @@ import android.util.Log;
 import java.lang.reflect.Type;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 
 import com.google.gson.JsonDeserializationContext;
@@ -17,13 +19,19 @@ public class DateDeserializer implements JsonDeserializer<Date> {
 
     @Override
     public Date deserialize(JsonElement element, Type arg1, JsonDeserializationContext arg2) throws JsonParseException {
-        String date = element.getAsString();
+        String dateText = element.getAsString();
 
-        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy H:mm a");
-        formatter.setTimeZone(TimeZone.getTimeZone("WEST"));
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy H:mm a", Locale.forLanguageTag("pt_PT"));
+
+        //formatter.setTimeZone(TimeZone.getTimeZone("WET"));
 
         try {
-            return formatter.parse(date);
+            Date date = formatter.parse(dateText);
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(date);
+            cal.add(Calendar.HOUR_OF_DAY, -1);
+            cal.add(Calendar.DAY_OF_MONTH, 1);
+            return cal.getTime();
         } catch (ParseException e) {
             Log.e("Failed to parse Date", e.getMessage());
             return null;
